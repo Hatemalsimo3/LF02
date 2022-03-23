d53ebc3ff63ddcf0143451ab344254d22d5d85b2
hatem3 <hatem3@schule.bremen.de>
2022-03-23 09:01:04 +0100

    testProgrammRead_CSV.java

--- Kaufvertrag/src/testProgrammRead_CSV.java
+++ Kaufvertrag/src/testProgrammRead_CSV.java
@@ -17,13 +17,12 @@ public class testProgrammRead_CSV {
             // verkaeufer
             if (array[0].equalsIgnoreCase("Verkaeufer")) {
                 String vorname = "";
-                String[] name = array[1].split("");
+                String[] name = array[1].split(" ");
                 for (int k = 0; k < name.length - 1; k++) {
                     vorname = vorname + name;
                     String nachname = name[name.length - 1];
                 }
                 Vertrapartner verkaeufer = new Vertrapartner(name[1], name[0]);
-
                 String strasse = "";
                 String[] strasseHausNr = array[2].split(" ");
                 String hausNr = strasseHausNr[strasseHausNr.length - 1];
@@ -32,29 +31,35 @@ public class testProgrammRead_CSV {
                 }
                 strasse.trim();
 
-                String plz1 = "";
-                String[] plz = array[3].split("  ");
-                String ort = plz[plz.length - 1];
-                for (int j = 1; j < plz.length; j++) {
-                    plz1 = plz1 + plz;
+                String[] plzort = array[3].split(" ");
+               String plz =plzort[0];
+               String ort="";
 
+                for (int j = 1; j < plzort.length; j++) {
+                    ort=plzort[j];
                 }
-                Adresse adresse = new Adresse(strasse, hausNr, plz1, ort);
+                ort.trim();
+
+                Adresse adresse = new Adresse(strasse, hausNr, plz, ort);
                 verkaeufer.setAdresse(adresse);
+                verkaeufer.getAusweisNr();
+                System.out.println(verkaeufer);
 
 
 // ---------------------------------------------------------------------------------------
 
-                String[] array1 = zeile.split(";");
+                String zeile2;
+                while ((zeile2 = br.readLine()) != null) {
+                String[] array1 = zeile2.split(";");
                 if (array1[0].equalsIgnoreCase("kaeufer")) {
+
                     String vorname1 = "";
-                    String[] name1 = array[1].split("");
+                    String[] name1 = array[1].split(" ");
                     for (int m = 0; m < name1.length - 1; m++) {
                         vorname1 = vorname1 + name1;
                         String nachname1 = name1[name1.length - 1];
                     }
-                    Vertrapartner kauefer = new Vertrapartner(name1[1], name1[0]);
-
+                    Vertrapartner kaeufer = new Vertrapartner(name1[1], name1[0]);
 
                     String strasse1 = "";
                     String[] strasseHausNr1 = array1[2].split(" ");
@@ -65,17 +70,22 @@ public class testProgrammRead_CSV {
                     }
                     strasse1.trim();
 
-                    String plz2 = "";
-                    String[] plz3 = array1[3].split("  ");
-                    String ort1 = plz3[plz3.length - 1];
-                    for (int v = 1; v < plz3.length; v++) {
-                        plz2 = plz2 + plz3;
 
+                    String[] plzort1 = array1[3].split(" ");
+                    String plz2=plzort1[0];
+
+                    String ort1 = "";
+                    for (int v = 1; v < plzort1.length-1; v++) {
+                      ort1 =plzort1[v];
                     }
-                    Adresse adresse1 = new Adresse(strasse1, hausNr1, plz1, ort1);
-                    kauefer.setAdresse(adresse1);
-                    System.out.println(adresse);
+                    ort1.trim();
 
+                    Adresse adresse1 = new Adresse(strasse1, hausNr1, plz2, ort1);
+                    kaeufer.setAdresse(adresse1);
+                    kaeufer.getAusweisNr();
+                    System.out.println(kaeufer);
+
+                }
                 }
 
 
